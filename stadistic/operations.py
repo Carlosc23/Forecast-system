6418992f00605274620ede063b5e5564d3b4db8a
Carlos <carlos.calderon097@gmail.com>
2017-11-27 00:47:03 -0600

    Fechas corregidas de nuevo.

--- stadistic/operations.py
+++ stadistic/operations.py
@@ -19,21 +19,24 @@ def smoothing():
     f2017 = []
     f2017 = averageM()[1]
     alpha = 0.74
-    d2017Suave.append(d2017M[0]) #obtener primer dato para el suavizamiento (es igual al del ponderado)
-    for i in range(len(f2017)-1):
-        d2017Suave.append(alpha*float(d2017M[i+1])+(1.0-alpha)*float(d2017Suave[i])) #ponderado junto con el del dia anterior
+    d2017Suave.append(d2017M[0])  # obtener primer dato para el suavizamiento (es igual al del ponderado)
+    for i in range(len(f2017) - 1):
+        d2017Suave.append(alpha * float(d2017M[i + 1]) + (1.0 - alpha) * float(
+            d2017Suave[i]))  # ponderado junto con el del dia anterior
     with open("stadistic/pronosticosSuaves.csv", "w") as csvDataFile:
         fieldnames = ['fecha', 'cobro']
         writer = csv.DictWriter(csvDataFile, fieldnames=fieldnames)
         writer.writeheader()
         for index in range(len(f2017)):
             writer.writerow({'fecha': str(f2017[index]), 'cobro': str(d2017Suave[index])})
+
+
 def averageM():
     fecha = []
     f2016 = []
     d2016 = []
-    f2017 = []
-    d2017 = []
+    f2015 = []
+    d2015 = []
     f2017 = []
     d2017 = []
     dinero = []
@@ -43,18 +46,19 @@ def averageM():
             fecha.append(row[0])
             dinero.append(row[1])
         for i in range(len(fecha)):
-            if "2017" in fecha[i]:
-                f2017.append(fecha[i])
-                d2017.append(dinero[i])
+            if "2015" in fecha[i]:
+                f2015.append(fecha[i])
+                d2015.append(dinero[i])
             elif "2016" in fecha[i]:
                 f2016.append(fecha[i])
                 d2016.append(dinero[i])
-        for j in range(len(f2017) - 3):
+        for j in range(len(f2015) - 3):
             if float(d2016[j + 1]) == 0:
-                d2017.append(float(d2017[j + 2]))
+                d2017.append(float(d2015[j + 2]))
             else:
-                d2017.append((3 * float(d2017[j + 2]) + 10 * float(d2016[j + 1])) / 13.0)  # linea donde se calcula el promedio/media ponderada
-            f2017.append(f2017[j + 1].replace("2017", "2017"))
+                d2017.append((3 * float(d2015[j + 2]) + 10 * float(
+                    d2016[j + 1])) / 13.0)  # linea donde se calcula el promedio/media ponderada
+            f2017.append(f2015[j + 1].replace("2015", "2017"))
         d2017.pop(0)
         f2017.pop(len(f2017) - 1)
     csvDataFile.closed
@@ -67,49 +71,50 @@ def averageM():
     csvDataFile.closed
     return d2017, f2017
 
+
 def createTable(numMes):
-    #print "estoy aqui"
+    # print "estoy aqui"
     opcion = numMes
-    tEnero = Table(names=("Fecha","Monto"), dtype=('S10','S11'))
-    tFebrero = Table(names=("Fecha", "Monto"), dtype=('S10','S11'))
-    tMarzo = Table(names=("Fecha", "Monto"), dtype=('S10','S11'))
-    tAbril = Table(names=("Fecha", "Monto"), dtype=('S10','S11'))
-    tMayo = Table(names=("Fecha", "Monto"), dtype=('S10','S11'))
-    tJunio = Table(names=("Fecha", "Monto"), dtype=('S10','S11'))
-    tJulio = Table(names=("Fecha", "Monto"), dtype=('S10','S11'))
-    tAgosto = Table(names=("Fecha", "Monto"), dtype=('S10','S11'))
-    tSeptiembre = Table(names=("Fecha", "Monto"), dtype=('S10','S11'))
-    tOctubre = Table(names=("Fecha", "Monto"), dtype=('S10','S11'))
-    tNoviembre = Table(names=("Fecha", "Monto"), dtype=('S10','S11'))
-    tDiciembre = Table(names=("Fecha", "Monto"), dtype=('S10','S11'))
+    tEnero = Table(names=("Fecha", "Monto"), dtype=('S10', 'f8'))
+    tFebrero = Table(names=("Fecha", "Monto"), dtype=('S10', 'f8'))
+    tMarzo = Table(names=("Fecha", "Monto"), dtype=('S10', 'f8'))
+    tAbril = Table(names=("Fecha", "Monto"), dtype=('S10', 'f8'))
+    tMayo = Table(names=("Fecha", "Monto"), dtype=('S10', 'f8'))
+    tJunio = Table(names=("Fecha", "Monto"), dtype=('S10', 'f8'))
+    tJulio = Table(names=("Fecha", "Monto"), dtype=('S10', 'f8'))
+    tAgosto = Table(names=("Fecha", "Monto"), dtype=('S10', 'f8'))
+    tSeptiembre = Table(names=("Fecha", "Monto"), dtype=('S10', 'f8'))
+    tOctubre = Table(names=("Fecha", "Monto"), dtype=('S10', 'f8'))
+    tNoviembre = Table(names=("Fecha", "Monto"), dtype=('S10', 'f8'))
+    tDiciembre = Table(names=("Fecha", "Monto"), dtype=('S10', 'f8'))
 
     with open("stadistic/pronosticosSuaves.csv") as csvDataFile:
         csvReader = csv.reader(csvDataFile, delimiter=",")
         for row in csvReader:
             if "2017-01" in row[0]:
-                tEnero.add_row((str(row[0]),"Q."+str(row[1])))
+                tEnero.add_row((str(row[0]), str(row[1])))
             elif "2017-02" in row[0]:
-                tFebrero.add_row((str(row[0]),"Q."+str(row[1])))
+                tFebrero.add_row((str(row[0]), row[1]))
             elif "2017-03" in row[0]:
-                tMarzo.add_row((row[0],"Q."+str(row[1])))
+                tMarzo.add_row((row[0], row[1]))
             elif "2017-04" in row[0]:
-                tAbril.add_row((str(row[0]),"Q."+str(row[1])))
+                tAbril.add_row((str(row[0]), row[1]))
             elif "2017-05" in row[0]:
-                tMayo.add_row((row[0],"Q."+str(row[1])))
+                tMayo.add_row((row[0], row[1]))
             elif "2017-06" in row[0]:
-                tJunio.add_row((row[0],"Q."+str(row[1])))
+                tJunio.add_row((row[0], row[1]))
             elif "2017-07" in row[0]:
-                tJulio.add_row((row[0],"Q."+str(row[1])))
+                tJulio.add_row((row[0], row[1]))
             elif "2017-08" in row[0]:
-                tAgosto.add_row((row[0],"Q."+str(row[1])))
+                tAgosto.add_row((row[0], row[1]))
             elif "2017-09" in row[0]:
-                tSeptiembre.add_row((row[0],"Q."+str(row[1])))
+                tSeptiembre.add_row((row[0], row[1]))
             elif "2017-10" in row[0]:
-                tOctubre.add_row((row[0],"Q."+str(row[1])))
+                tOctubre.add_row((row[0], row[1]))
             elif "2017-11" in row[0]:
-                tNoviembre.add_row((row[0],"Q."+str(row[1])))
+                tNoviembre.add_row((row[0], row[1]))
             elif "2017-12" in row[0]:
-                tDiciembre.add_row((row[0],"Q."+str(row[1])))
+                tDiciembre.add_row((row[0], row[1]))
     csvDataFile.closed
     if opcion == "1":
         return tEnero
@@ -136,83 +141,84 @@ def createTable(numMes):
     elif opcion == "12":
         return tDiciembre
 
+
 def graficarConSuavizamiento(numMes):
     pre = os.path.dirname(os.path.realpath(__file__))
     path = os.path.join(pre, 'pronosticosSuaves.csv')
     series = read_csv(path, header=0, parse_dates=[0], index_col=0, squeeze=True)
-    tiempo=""
+    tiempo = ""
     tiempo = "2017-0" + str(numMes)
     print tiempo
     a = series[tiempo]
     plt.figure(1)
-    #plt.subplot(211)
-    #a.hist(xlabelsize=10,ylabelsize=10,bins=5)
-    a.plot(color=plt.rcParams['axes.color_cycle'][1],linewidth=1.5, linestyle="-")
+    # plt.subplot(211)
+    # a.hist(xlabelsize=10,ylabelsize=10,bins=5)
+    a.plot(color=plt.rcParams['axes.color_cycle'][1], linewidth=1.5, linestyle="-")
     # Font style isn't accessible through rcParams
-    #plt.set_title("Product A sales", fontstyle='italic')
+    # plt.set_title("Product A sales", fontstyle='italic')
     # Set an aspect ratio
-    #plt.savefig('static/image/Graph.png')
+    # plt.savefig('static/image/Graph.png')
     if numMes == "1":
-        
+
         plt.savefig('static/image/Graph1.png')
         plt.cla()
         plt.close()
-        path='Graph1.PNG'
+        path = 'Graph1.PNG'
     elif numMes == "2":
         plt.savefig('static/image/Graph2.png')
         plt.cla()
         plt.close()
-        path='Graph2.PNG'
+        path = 'Graph2.PNG'
     elif numMes == "3":
         plt.savefig('static/image/Graph3.png')
         plt.cla()
         plt.close()
-        path='Graph3.PNG'
+        path = 'Graph3.PNG'
     elif numMes == "4":
         plt.savefig('static/image/Graph4.png')
         plt.cla()
         plt.close()
-        path='Graph4.PNG'
+        path = 'Graph4.PNG'
     elif numMes == "5":
         plt.savefig('static/image/Graph5.png')
         plt.cla()
         plt.close()
-        path='Graph5.PNG'
+        path = 'Graph5.PNG'
     elif numMes == "6":
         plt.savefig('static/image/Graph6.png')
         plt.cla()
         plt.close()
-        path='Graph6.PNG'
+        path = 'Graph6.PNG'
     elif numMes == "7":
         plt.savefig('static/image/Graph7.png')
         plt.cla()
         plt.close()
-        path='Graph7.png'
+        path = 'Graph7.png'
     elif numMes == "8":
         plt.savefig('static/image/Graph8.png')
         plt.cla()
         plt.close()
-        path='Graph8.png'
+        path = 'Graph8.png'
     elif numMes == "9":
         plt.savefig('static/image/Graph9.png')
         plt.cla()
         plt.close()
-        path='Graph9.png'
+        path = 'Graph9.png'
     elif numMes == "10":
         plt.savefig('static/image/Graph10.png')
         plt.cla()
         plt.close()
-        path='Graph10.png'
+        path = 'Graph10.png'
     elif numMes == "11":
         plt.savefig('static/image/Graph11.png')
         plt.cla()
         plt.close()
-        path='Graph11.png'
+        path = 'Graph11.png'
     elif numMes == "12":
         plt.savefig('static/image/Graph12.png')
         plt.cla()
         plt.close()
-        path='Graph12.png'
+        path = 'Graph12.png'
     return path
 
 
@@ -226,6 +232,7 @@ def generate_graphs():
     # ax1.plot(series['2017-02'], color=plt.rcParams['axes.color_cycle'][2], linewidth=1.5, linestyle="-")
     # ax1.plot(series['2017-03'], color=plt.rcParams['axes.color_cycle'][2], linewidth=1.5, linestyle="-")
     # plt.show()
-    plt.savefig('static/image/Graph.png')
+    plt.savefig('static/images/Graph.png')
     path = 'Graph.png'
     return path
+
